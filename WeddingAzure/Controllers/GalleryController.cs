4f8f7632bea225f8354c2efe84488b249f2b8079
Andy Sprague <andy.sprague44@gmail.com>
2018-07-07 18:39:17 +0000

    Gallery placeholder

---
+++ WeddingAzure/Controllers/GalleryController.cs
@@ -0,0 +1,35 @@
+﻿using Sprague.Andy.WeddingAzure.DataAccess.Rsvp;
+using Sprague.Andy.WeddingAzure.Models.Rsvp;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Serilog;
+using Sprague.Andy.WeddingAzure.DataAccess;
+using Sprague.Andy.WeddingAzure.DataAccess.Mail;
+
+namespace WeddingAzure.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public static ILogger _log = Log.ForContext<GalleryController>();
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Gallery.";
+
+            return View();
+        }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+
+            //Log the error!!
+            _log.Error(filterContext.Exception.Message);
+
+            filterContext.Result = new ViewResult
+            {
+                ViewName = "~/Views/Shared/Error.cshtml"
+            };
+        }
+    }
+}
